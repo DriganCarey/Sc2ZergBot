0271d659c9ead52c317d303ced3564b8e5c1d0a4
DriganCarey <42607780+DriganCarey@users.noreply.github.com>
2018-08-22 12:07:17 +0100

    EmptyBot added
    Added a file containing a basic bot that currently does nothing

---
+++ EmptyBot.py
@@ -0,0 +1,12 @@
+import sc2
+from sc2 import Race, Difficulty, maps, run_game
+from sc2.constants import *
+from sc2.player import Bot, Computer
+from sc2.data import race_townhalls
+
+class ZergBot(sc2.BotAI):
+	def __init__(self):
+		pass
+
+	async def on_step(self, iteration):
+		pass
