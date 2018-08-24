ec9bbc0c77cf3b89e65f2f2e39b1744e46b0d921
Drigan Carey <Drigan7894Carey@outlook.com>
2018-08-24 08:57:17 +0100

    Minor edit
    Empty bot will now run a game using the bot vs an inbuilt ai when run

--- EmptyBot.py
+++ EmptyBot.py
@@ -10,3 +10,9 @@ class ZergBot(sc2.BotAI):
 
 	async def on_step(self, iteration):
 		pass
+
+#running the game:
+# first parameter is the map
+# second parameter is the list of players/bots
+# third is whether the game should be in a realtime, or sped up
+run_game(maps.get("AbyssalReefLE"), [Bot(Race.Zerg, ZergBot()), Computer(Race.Zerg, Difficulty.Easy)], realtime = True)
