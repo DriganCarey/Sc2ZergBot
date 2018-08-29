f114619f9019bd1afe048b9eb8ab7f03ff949db8
Drigan Carey <Drigan7894Carey@outlook.com>
2018-08-29 06:23:05 +0100

    Abandoning TestBot and forcusing on BotOne
    Following Part 2 in Python AI in StarCraft II

---
+++ BotOne.py
@@ -0,0 +1,91 @@
+from functools import reduce
+from operator import or_
+import random
+
+import sc2
+from sc2 import Race, Difficulty, maps, run_game
+from sc2.constants import *
+from sc2.player import Bot, Computer
+from sc2.data import race_townhalls
+
+
+class ZergBotV2(sc2.BotAI):
+	def __init__(self):
+		self.drone_count = 0
+		self.unit_production_index = 0
+		self.unit_production_list = ["DRONE"]
+		self.unit_production = []
+
+	# GET ALL AVALIABLE VESPENE GEYSERS
+	# gets a list of vespene geysers that aren't occupied, are next to a finished hatchery and aren't empty
+	# DOESNT CHECK IF THERE ARE EMPTY and whether are blocked
+	def get_avaliable_gysers(self):
+		# creates a list of viable gysers near hatcheries
+		vespeneGeysers = []
+		for hatchery in self.units(HATCHERY):
+			for vg in self.state.vespene_geyser.closer_than(10, hatchery):
+				# checking if the vg is already occupied
+				if not (self.units(EXTRACTOR).closer_than(1.0, vg).exists or self.units(ASSIMILATOR).closer_than(1.0, vg).exists or self.units(REFINERY).closer_than(1.0, vg).exists):
+					vespeneGeysers.append(vg)
+		return vespeneGeysers
+
+	#variable definitions:
+	@property
+	def get_unsaturated_bases(self) -> "Units":
+		return self.townhalls.filter(lambda x: x.assigned_harvesters < x.ideal_harvesters)
+
+	@property
+	def get_oversaturated_bases(self) -> "Units":
+		return self.townhalls.filter(lambda x: x.assigned_harvesters > x.ideal_harvesters)
+
+	async def manage_drones(self):
+		# MANANGING DRONES:
+		# idle drones
+		for idle_worker in self.units(DRONE).idle:
+			if (self.get_unsaturated_bases.exists):
+				target_hatch = self.get_unsaturated_bases.closest_to(idle_worker)
+			else:
+				target_hatch = self.townhalls.closest_to(idle_worker)
+			# checking if there is an unsaturated base and assigning worker to closest one
+			await self.do(idle_worker.gather(self.state.mineral_field.closest_to(target_hatch)))
+			break
+		# oversaturated mineral lines (only manages if there are un-saturated bases)
+		if not self.drone_saturation:
+			for original_hatch in self.get_oversaturated_bases():
+				for random_worker in self.workers.closer_than(17, original_hatch):
+					target_hatch = self.get_unsaturated_bases.closest_to(original_hatch)
+					await self.do(random_worker.gather(self.state.mineral_field.closest_to(target_hatch)))
+					break
+
+	async def build_units(self):
+		if self.units(LARVA).exists and all(self.unit_production):
+			if self.unit_production_index < len(self.unit_production_list):
+				current_unit = self.unit_production_list[self.unit_production_index]
+				if current_unit == "DRONE":
+					if self.can_afford(DRONE) and self.supply_left >= 1:
+						await self.do(self.units(LARVA).random.train(DRONE))
+			else:
+				self.unit_production_index = 0
+
+	async def build_overlords(self):
+		if self.supply_left < 5 and not self.already_pending(OVERLORD):
+			if self.units(LARVA).exists and self.can_afford(OVERLORD):
+				await self.do(self.units(LARVA).random.train(OVERLORD))
+
+	async def on_step(self, iteration):
+		# updating variables
+		self.drone_count = self.units(DRONE).amount + self.already_pending(DRONE)
+		self.unit_production = [True]
+		
+		await self.manage_drones()
+		await self.build_overlords()
+		await self.build_units()
+		
+		
+	
+#running the game:
+# first parameter is the map
+# second parameter is the list of players/bots
+# third is whether the game should be in a realtime, or sped up
+run_game(maps.get("AbyssalReefLE"), [Bot(Race.Zerg, ZergBotV2()), Computer(Race.Zerg, Difficulty.Easy)], realtime = True)
+
--- TestBot.py
+++ TestBot.py
@@ -13,6 +13,7 @@ class TestBot(sc2.BotAI):
 		self.ITERATIONS_PER_SECOND = 2.75
 		self.hatcherys = []
 		self.last_variable_update = None
+		self.production = ["DRONE"]
 
 	# ADDING HATCHERY TO MY LIST
 	def add_hatchery(self, input_hatch: Unit, matching_queen: Unit = None):
@@ -25,13 +26,61 @@ class TestBot(sc2.BotAI):
 	def get_larva(self, hatchery: Unit) -> Units:
 		return self.units(LARVA).closer_than(5, hatchery)
 
+	def predict_larva(self, time: int) -> int:
+		larva_count = 0
+		for hatch in self.hatcherys:
+			# current larva
+			current_hatch_larva_count = self.get_larva(hatch).amount
+			# larva count including naturally spawned larva
+			current_hatch_larva_count += time/11
+			current_hatch_larva_count = min(current_hatch_larva_count, 3)
+			# larva count including popped larva
+			if len(hatch[2] > 0):
+					number_of_popped_injects = 0
+					if time >= hatch[2][0]:
+						number_of_popped_injects += 1
+						number_of_popped_injects += min((time - hatch[2][0])/29, len(hatch[2]) - 1)
+						current_hatch_larva_count += 3*number_of_popped_injects
+			current_hatch_larva_count = min(current_hatch_larva_count, 19)
+		return current_hatch_larva_count
+
+	def predict_needed_supply(self, time: int) ->int:
+		future_larva_count = self.predict_larva(18)
+		production_loop = len(self.production)
+		needed_supply = 0
+		for x in range(0, future_larva_count):
+			unit_needed = self.production[x%production_loop]
+			if unit_needed == "DRONE":
+				needed_supply += 1
+		return needed_supply
+
+	def get_to_be_avaliable_supply(self):
+		# BUILD TIMES
+		overlord_build_time = 18 #seconds
+		float_done_hatch = 0.7465 #if hatch progress is this much, it will finish in 18 seconds
+		# SUPPLY GIVEN BY
+		hatchery_supply = 6 #seconds
+		overlord_supply = 8 #seconds
+
+		# calculating supply left in 18 seconds
+		already_built_supply = self.already_pending(OVERLORD) * overlord_supply
+		avaliable_supply = self.supply_left + (hatchery_supply * self.townhalls.filter(lambda h: 1 > h.build_progress > float_done_hatch).amount) + already_built_supply
+
+		return avaliable_supply
+
 	async def on_step(self, iteration):
+		# only at the beginning of the game
 		if (iteration == 0):
 			for hatch in self.units(HATCHERY):
 				self.add_hatchery(hatch)
 			self.last_variable_update = 0
+			self.hatcherys[0][2].append(29)
 
-		if self.time >= self.last_variable_update + 1:
+		# updating certain variables every 3 iterations
+		current_time = self.time
+
+		# updating queued injects every second
+		if current_time >= self.last_variable_update + 1:
 			for hatch in self.hatcherys:
 				# if there is a queued inject
 				if len(hatch[2]) > 0:
@@ -39,13 +88,25 @@ class TestBot(sc2.BotAI):
 						hatch[2][0] += -1
 					else:
 						del hatch[2][0]
-			self.last_variable_update = self.time
+			self.last_variable_update = current_time
 
-
+		if iteration%3 == 0:
 			print("self.time: " + str(self.time))
 			print("Larva no: " + str(self.units(LARVA).closer_than(5, self.hatcherys[0][0]).amount))
-			print("Inject queue: " + str(self.hatcherys[0][3]))
-			
+			print("Inject queue: " + str(self.hatcherys[0][2]))
+		
+		# macro cycle
+		if current_time%18 == 0:
+			# OVERLORD
+			needed_supply = self.predict_needed_supply(18)
+			will_be_supply = self.get_to_be_avaliable_supply()
+			diff = needed_supply - will_be_supply
+			if (diff > 0):
+				if self.can_afford(OVERLORD) and self.units(LARVA).exists:
+					err = await self.do(self.units(LARVA).random.train(OVERLORD))
+			# UNITS
+			# INJECT
+			# SPREAD CREEP
 
 
 			
